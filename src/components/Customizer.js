35d31cac75d201e0fd5d9a310e36860814feaf30
intrvertmichael <michaelpaguay@gmail.com>
2020-04-18 15:33:46 -0400

    finished main ui

--- src/components/Customizer.js
+++ src/components/Customizer.js
@@ -5,18 +5,22 @@ const customizer = () =>{
   return (
     <div className='customizer'>
       <div className='customizer-content'>
-        <div> <h1>Custom Colors: </h1> </div>
         <div className='customizer-colors'>
-          <div className='customizer-color'>custom <div className='color'></div></div>
-          <div className='customizer-color'>custom <div className='color'></div></div>
-          <div className='customizer-color'>custom <div className='color'></div></div>
-          <div className='customizer-color'>custom <div className='color'></div></div>
-          <div className='customizer-color'>custom <div className='color'></div></div>
-          <div className='customizer-color'>custom <div className='color'></div></div>
+          <div className='customizer-color-label'> Custom Colors: </div>
+          <div className='customizer-color'>Background <div className='color background' onClick={()=>customColor('background')}></div> </div>
+          <div className='customizer-color'>Text <div className='color text' onClick={()=>customColor('Text')}></div> </div>
+          <div className='customizer-color'>Title <div className='color title' onClick={()=>customColor('Title')}></div> </div>
+          <div className='customizer-color'>Quotes <div className='color quotes' onClick={()=>customColor('Quotes')}></div> </div>
+          <div className='customizer-color'>Highlights <div className='color highlights' onClick={()=>customColor('Highlights')}></div> </div>
+          <div className='customizer-color reset' onClick={()=>customColor('Reset')}> Reset </div>
         </div>
       </div>
     </div>
   )
+}
+
+const customColor = link => {
+  console.log(`custom ${link} color clicked`);
 }
 
 export default customizer;
